8942b81b77b36151110723c29f9eaf0d67a9efb5
He Zhao <james_littlefox@hotmail.com>
2014-08-14 00:42:03 -0700

    save user_id, access_token, refresh_token to redis

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
+import os
+from store import Store
+from datetime import datetime, timedelta
 from flask import Flask, url_for, request, session, redirect
 from moves import MovesClient
-from datetime import datetime, timedelta
-import os
-# import _keys
+
 
 app = Flask(__name__)
 
-# client_id = '3nZfgFy7wdzXNp9wd2PnglvysqzJUcox'
-# client_secret = 'QPasM3H4_5iX91WJSitTOBi6aRHmByu4s38t9_2YewBDy_tYVT0LKFhEc8Bj4mUn'
-client_id = os.environ['CLIENT_ID']
-client_secret = os.environ['CLIENT_SECRET']
+# config vars
+client_id       = os.environ['CLIENT_ID']
+client_secret   = os.environ['CLIENT_SECRET']
+redis_url       = os.environ['REDISTOGO_URL']
 
-Moves = MovesClient(client_id, client_secret)
-
-access_token = '2bE1LPx7l41a8IJ8YKhA9Fe15i12ZbaXs29DMdhhRs6MfzGbkCNw7315ELl4lIlJ'
-user_id = '66863717373915380'
+# class instances
+moves           = MovesClient(client_id, client_secret)
+store           = Store(redis_url)
 
 @app.route("/")
 def index():
     if 'access_token' not in session:
         oauth_return_url = url_for('oauth_return', _external=True)
-        auth_url = Moves.build_oauth_url(oauth_return_url)
+        auth_url = moves.build_oauth_url(oauth_return_url)
         return 'Authorize this application: <a href="%s">%s</a>' % \
             (auth_url, auth_url)
     return redirect(url_for('show_info'))
 
 @app.route("/myinfo")
 def myinfo():
-    profile = Moves.user_profile(access_token=access_token)
+    profile = moves.user_profile(access_token=access_token)
     response = 'User ID: %s<br />First day using Moves: %s' % \
         (profile['userId'], profile['profile']['firstDate'])
     return response + "<br /><a href=\"%s\">Info for today</a>" % url_for('today') + \
@@ -41,10 +41,18 @@ def oauth_return():
         return error
     oauth_return_url = url_for('oauth_return', _external=True)
     code = request.args.get("code")
-    response = Moves.get_oauth_token(code, redirect_uri=oauth_return_url)
-    print response
+    response = moves.get_oauth_token(code, redirect_uri=oauth_return_url)
+    
+    # store access_token and user_id in session
     session['access_token'] = response['access_token']
-    session['refresh_token'] = response['refresh_token']
+    session['user_id'] = response['user_id']
+
+    # user:[user_id] = access_token
+    store.set_access_token(session['user_id'], response['access_token'])
+
+    # user:[user_id] = refresh_token
+    store.set_refresh_token(session['user_id'], response['refresh_token'])
+
     return redirect(url_for('show_info'))
 
 
@@ -57,7 +65,7 @@ def logout():
 
 @app.route("/info")
 def show_info():
-    profile = Moves.user_profile(access_token=session['access_token'])
+    profile = moves.user_profile(access_token=session['access_token'])
     response = 'User ID: %s<br />First day using Moves: %s' % \
         (profile['userId'], profile['profile']['firstDate'])
     return response + "<br /><a href=\"%s\">Info for today</a>" % url_for('today') + \
@@ -67,7 +75,7 @@ def show_info():
 @app.route("/today")
 def today():
     today = datetime.now().strftime('%Y%m%d')
-    info = Moves.user_summary_daily(today, access_token=session['access_token'])
+    info = moves.user_summary_daily(today, access_token=session['access_token'])
     res = ''
     if info[0]['summary'] is None:
         return res
