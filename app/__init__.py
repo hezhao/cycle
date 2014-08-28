e9b793450ac03839605b62ad7cf66f6bee16b5ab
hezhao <zhao.he@wk.com>
2014-08-28 12:29:47 -0700

    use Flask-SSLify in production mode

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from app.views import views as views
+from flask_sslify import SSLify
 
 # flask
 app             = Flask(__name__)
 app.secret_key  = os.environ['FLASK_APP_SECRET']
 app.register_blueprint(views)
+sslify          = SSLify(app)
